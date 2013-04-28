2dfac542aeaa383a4cecb36cc0db17c01a2c609c
Justin Obara <obarajustin@gmail.com>
2013-04-28 15:44:10 -0400

    Added concatenations

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,12 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: ["lib/jquery-1.9.1.min.js", "js/fontIconTest.js"],
+                dest: "build/fontIconTest-concat.js"
+            }
+        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
@@ -8,13 +14,14 @@ module.exports = function (grunt) {
             },
             my_target: {
                 files: {
-                    "build/fontIconTest.min.js": ["js/fontIconTest.js"]
+                    "build/fontIconTest.min.js": ["build/fontIconTest-concat.js"]
                 }
             }
         }
     });
 
+    grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-uglify");
 
-    grunt.registerTask("default", ["uglify"]);
+    grunt.registerTask("default", ["concat", "uglify"]);
 };
